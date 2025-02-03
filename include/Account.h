fdc4a5e7858875ef65f7c1eb1f4aa89d5e7ecb16
joshuang87 <joshuang43wq@gmail.com>
2025-02-03 23:32:44 +0800

    [opt] Add code comment

--- include/Account.h
+++ include/Account.h
@@ -1,3 +1,8 @@
+/**
+ * @file Account.h
+ * @brief Header file for the Account class which handles user authentication
+ */
+
 #ifndef ACCOUNT_H
 #define ACCOUNT_H
 
@@ -6,30 +11,86 @@
 
 using namespace std;
 
+/**
+ * @class Account
+ * @brief Represents a user account with username and password
+ * 
+ * This class handles user account operations including creation, authentication,
+ * and JSON serialization/deserialization.
+ */
 class Account {
     private:
-        string username;
-        string password;
+        string username;  ///< The username of the account
+        string password;  ///< The password of the account
 
     public:
+        /**
+         * @brief Default constructor
+         * Initializes an empty account with empty username and password
+         */
         Account() : username(""), password("") {}
+        
+        /**
+         * @brief Parameterized constructor
+         * @param user The username for the account
+         * @param pwd The password for the account
+         */
         Account(string user, string pwd);
 
-        // Getters
+        /**
+         * @brief Get the username of the account
+         * @return The username as a string
+         */
         string getUsername() const;
+
+        /**
+         * @brief Get the password of the account
+         * @return The password as a string
+         */
         string getPassword() const;
 
-        // Setters
+        /**
+         * @brief Set the username of the account
+         * @param user The new username to set
+         */
         void setUsername(string user);
+
+        /**
+         * @brief Set the password of the account
+         * @param pwd The new password to set
+         */
         void setPassword(string pwd);
 
+        /**
+         * @brief Create a new account in the database
+         * @param acc The account to create
+         * @return true if account creation was successful, false if account already exists
+         */
         static bool create(const Account& acc);
+
+        /**
+         * @brief Check if an account exists in the database
+         * @param acc The account to check
+         * @return true if account exists, false otherwise
+         */
         static bool check(const Account& acc);
 
-        // JSON parsing
+        /**
+         * @brief Parse a JSON string into a vector of Account objects
+         * @param json The JSON string to parse
+         * @return Vector of Account objects
+         */
         static vector<Account> from_json(const string& json);
+
+        /**
+         * @brief Convert the account to a JSON string
+         * @return JSON string representation of the account
+         */
         string to_json() const;
 
+        /**
+         * @brief Default destructor
+         */
         ~Account() = default;
 };
 
--- src/Account.cpp
+++ src/Account.cpp
@@ -1,35 +1,74 @@
+/**
+ * @file Account.cpp
+ * @brief Implementation of the Account class methods
+ */
+
 #include "../include/Account.h"
 #include "../include/DB.h"
 
 #include <fstream>
 #include <sstream>
 
+/**
+ * @brief Constructs an Account object with given username and password
+ * @param user The username for the account
+ * @param pwd The password for the account
+ */
 Account::Account(string user, string pwd) {
     username = user;
     password = pwd;
 }
 
+/**
+ * @brief Gets the username of the account
+ * @return The username as a string
+ */
 string Account::getUsername() const {
     return username;
 }
 
+/**
+ * @brief Gets the password of the account
+ * @return The password as a string
+ */
 string Account::getPassword() const {
     return password;
 }
 
+/**
+ * @brief Sets the username of the account
+ * @param user The new username to set
+ */
 void Account::setUsername(string user) {
     this->username = user;
 }
 
+/**
+ * @brief Sets the password of the account
+ * @param pwd The new password to set
+ */
 void Account::setPassword(string pwd) {
     this->password = pwd;
 }
 
+/**
+ * @brief Creates a new account in the database
+ * @param acc The account to create
+ * @return true if account creation was successful, false if account already exists
+ */
 bool Account::create(const Account& acc) {
     if (check(acc)) return false;
     return DB::getInstance().save(acc);
 }
 
+/**
+ * @brief Checks if an account exists in the database
+ * @param acc The account to check
+ * @return true if account exists, false otherwise
+ * 
+ * This method loads all accounts from the database and checks if there's
+ * a matching username and password combination.
+ */
 bool Account::check(const Account& acc) {
     vector<Account> accList = DB::getInstance().load<Account>();
     if (accList.empty()) {
@@ -44,6 +83,15 @@ bool Account::check(const Account& acc) {
     return false;
 }
 
+/**
+ * @brief Parses a JSON string into a vector of Account objects
+ * @param json The JSON string to parse
+ * @return Vector of Account objects
+ * 
+ * This method parses a JSON string containing account information and
+ * creates Account objects from it. The JSON format should be:
+ * {"username":"value","password":"value"}
+ */
 vector<Account> Account::from_json(const string& json) {
     vector<Account> accounts;
     stringstream ss(json);
@@ -72,6 +120,11 @@ vector<Account> Account::from_json(const string& json) {
     return accounts;
 }
 
+/**
+ * @brief Converts the account to a JSON string
+ * @return JSON string representation of the account in the format:
+ *         {"username":"value","password":"value"}
+ */
 string Account::to_json() const {
     return "{\"username\":\"" + username + "\",\"password\":\"" + password + "\"}";
 }
